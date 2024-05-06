7dd88f7e08b4140ae4a99292b265050bddfcdb7d
luunhatthanhcnttk61 <6151071101@st.utc2.edu.vn>
2024-05-07 00:00:31 +0700

    update Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,6 +14,11 @@ public class CameraController : MonoBehaviour
 
     public Transform pivot;
 
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,8 +42,24 @@ public class CameraController : MonoBehaviour
 
         //Get Y position of mouse and rotate target
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(vertical, 0, 0);
+        //pivot.Rotate(vertical, 0, 0);
+        if(invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }    
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
 
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
         //Move camera
         float desiredYAngle = target.eulerAngles.y;
         float desiredXAngle = pivot.eulerAngles.x;
